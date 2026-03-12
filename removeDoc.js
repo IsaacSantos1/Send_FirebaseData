1cceafcd12de0eb6d80123a60b6bd20984c931b9
IsaacSantos1 <isaacsantos0902@gmail.com>
2026-03-12 08:31:47 -0400

    New

--- removeDoc.js
+++ removeDoc.js
@@ -1,6 +1,5 @@
 import {db} from './firebase-config.js';
-import { doc, deleteDoc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
-
+import { doc, deleteDoc, updateDoc, deleteField } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 // Now we want to turn this into a function that will be triggered on the buttonClick in a html page
 // We want to first grab the button click and then call this function to delete the document with id "delete1" from the "orders" collection in Firestore
@@ -28,6 +27,21 @@ deleteButton.addEventListener("click", async () => {
 
 });
 
+//const button =document.getElementById("del1");
+//button.addEventListener("click", removeDoc1);
 
-// const button =document.getElementById("del1");
-// button.addEventListener("click", removeDoc1);
+// Now we  want to focus on deletoing a speciic field fr, a document, not the whole document itsle
+export async function removeField(){
+    try{
+    const delRef= doc(db, "orders", "order1");
+    await updateDoc(delRef, {
+        studentId:deleteField()
+    });
+    alert("Field deleted successfully");
+}catch(error){
+    console.error("Error deleting field: ", error);
+}
+}
+
+const button2= document.getElementById("deleteFields");
+button2.addEventListener("click", removeField);
